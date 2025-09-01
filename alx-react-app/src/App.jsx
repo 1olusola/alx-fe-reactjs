ffb2fa3bd72322ed829abfaf12aad38c1fb7e381
Olusola Ogunwobi <olusolaogunwobi@gmail.com>
2025-09-01 02:20:04 +0100

    Update to Create a User Profile Card Using Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 
   return (
     <>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Header />
       <MainContent />
       <Footer />
