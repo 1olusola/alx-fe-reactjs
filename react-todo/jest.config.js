a5f5d2685e8b935275369f6b5b7bb2cc83ce21ba
Olusola Ogunwobi <olusolaogunwobi@gmail.com>
2025-10-19 23:10:53 +0100

    Update jest.config.js

--- react-todo/jest.config.js
+++ react-todo/jest.config.js
@@ -1,10 +1,5 @@
 export default {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  moduleNameMapping: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
+  transform: {},
 };
