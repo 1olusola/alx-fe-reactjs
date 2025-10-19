c9afd0227a5da966f097115c39a5fa940397e9d2
Olusola Ogunwobi <olusolaogunwobi@gmail.com>
2025-10-19 22:39:12 +0100

    Update App.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,7 +1,13 @@
-import TodoList from "./components/TodoList";
-export default function App() {
-return (
-<div><h1>
-<TodoList />
-);
+import React from 'react';
+import TodoList from './components/TodoList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
 }
+
+export default App;
