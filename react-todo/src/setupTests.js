460d60b1921cf87fc888f22080eba58db3fc3b64
Olusola Ogunwobi <olusolaogunwobi@gmail.com>
2025-10-19 23:02:52 +0100

    Update setupTests.js

--- react-todo/src/setupTests.js
+++ react-todo/src/setupTests.js
@@ -1,16 +1,19 @@
 import '@testing-library/jest-dom';
 
-// Mock window.matchMedia
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
+// Mock console.errors to reduce test noise
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (
+      /Warning: ReactDOM.render is deprecated/.test(args[0]) ||
+      /Warning:.*not wrapped in act/.test(args[0])
+    ) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
 });
