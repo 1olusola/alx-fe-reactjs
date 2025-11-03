fbe2812a0379f9aff772b502f93f9c7b3a76bef9
Olusola Ogunwobi <olusolaogunwobi@gmail.com>
2025-11-03 12:08:07 +0100

    Refactor DeleteRecipeButton to handle navigation

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,11 +1,14 @@
+import { useNavigate } from 'react-router-dom'
 import useRecipeStore from './recipeStore'
 
 export default function DeleteRecipeButton({ id, onDeleted }) {
   const del = useRecipeStore((s) => s.deleteRecipe)
+  const navigate = useNavigate()
 
   const handle = () => {
     del(id)
-    onDeleted?.()
+    if (onDeleted) onDeleted()
+    else navigate('/')
   }
 
   return (
