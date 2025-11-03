0f84a3a773952b1b330321f96e7c90dbc174d56e
Olusola Ogunwobi <olusolaogunwobi@gmail.com>
2025-11-03 12:00:23 +0100

    Fix import path for recipeStore in RecipeList

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import useRecipeStore from '../store/recipeStore'
+import useRecipeStore from './recipeStore'
 import SearchBar from './SearchBar'
 
 export default function RecipeList() {
